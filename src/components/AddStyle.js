2ed1dd459859a4876866c2f7c94441683add13da
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-10-16 10:22:18 +0300

    done

--- src/components/AddStyle.js
+++ src/components/AddStyle.js
@@ -71,7 +71,7 @@ function AddStyle() {
         ></textarea>
         <br />
         <label htmlFor='successrate'>
-          Success rate <em>(out of 10)</em>
+          Success Rate <em>(out of 10)</em>
         </label>
         <br />
         <input
