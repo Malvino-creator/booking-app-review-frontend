c4b104068e537fd59e1904893877910032b89b22
Moringa School <malvin.opondo@student.moringaschool.com>
2022-12-13 12:33:47 +0300

    Initial Commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import hair from '../img/hair.png';
-import cornrow from '../img/corn-row.jpeg';
-import weaving from "../img/weaving.jpeg"
-import coloring from "../img/coloring.jpeg"
-import homebook from "../img/home-book.png"
+import { Link } from 'react-router-dom'
+import download1 from '../img/download1.jpeg'
+import download2 from '../img/download2.jpeg'
+import download3 from "../img/download3.jpeg"
+import download4 from "../img/download4.webp"
+import download6 from "../img/download6.jpeg"
+
 
 function Home() {
   return (
     <>
       <div className='home-cont'>
         <div className='home-cont-cont'>
-          <h5>Your Amazing</h5>
+          <h5>Your Perfect</h5>
           <br />
-          <h1>New Look</h1>
+          <h1>Stress Reliever</h1>
           <br />
           <p>
-            Welcome to Hair Saloon. Lorem ipsum dolor sit amet consectetur
+            Welcome to Octah Spa. Lorem ipsum dolor sit amet consectetur
             adipisicing elit. Fugit repellat veniam laborum fuga architecto unde
             maiores rerum deleniti quibusdam, vitae, nobis iusto accusantium
             soluta sit ipsam nam doloribus eaque dolor.
           </p>
           <br />
-          <Link to='/beauty'>
-            <button type='button'>See styles</button>
-          </Link>
         </div>
         <div className='home-cont-img'>
-          <img src={hair} alt='hair' />
+          <img src={download1} alt='download1' />
         </div>
       </div>
       <div className='home-cards'>
         <div className='home-card'>
-          <img src={cornrow} alt='cornrow' />
+          <img src={download2} alt='download2' />
           <h2>Corn Rows</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
@@ -46,7 +44,7 @@ function Home() {
           </Link>
         </div>
         <div className='home-card'>
-          <img src={weaving} alt='cornrow' />
+          <img src={download3} alt='download3' />
           <h2>Weaving</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
@@ -60,7 +58,7 @@ function Home() {
           </Link>
         </div>
         <div className='home-card'>
-          <img src={coloring} alt='cornrow' />
+          <img src={download6} alt='download6' />
           <h2>Coloring</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
@@ -72,25 +70,6 @@ function Home() {
           <Link to='/' className='card-link'>
             <h4>Visit us today 》</h4>
           </Link>
-        </div>
-      </div>
-      <div className='home-book-main'>
-        <div className='home-book'>
-          <div className='home-book-cont'>
-            <p>
-              Welcome to Hair Saloon. Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Fugit repellat veniam laborum fuga architecto
-              unde maiores rerum deleniti quibusdam, vitae, nobis iusto
-              accusantium soluta sit ipsam nam doloribus eaque dolor.
-            </p>
-            <br />
-            <Link to='/addstyle'>
-              <button type='button'>SEE STYLES</button>
-            </Link>
-          </div>
-          <div className='home-book-img'>
-            <img src={homebook} alt='hair' />
-          </div>
         </div>
       </div>
     </>
